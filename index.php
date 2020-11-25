84b09bd1b600f789ac926e0a978891a4baf62328
= <=>
2020-11-25 13:47:02 +0700

    them anh

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onmi Food</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+    <header>
+        <div class="topnav" >
+            <a href="">FOOD DELIVERY</a>
+            <a href="">HOW IT WORKS</a>
+            <a href="">OUR CITIES</a>
+            <a href="">SIGN UP</a>
+        </div>
+        <img src="image/logo-white.png" alt="logo" width="100vh" height="100vh" class="logo">
+            
+        <div class="hd">
+            <H1 class="whiteh">GOODBYE JUNK FOOD. <br>HELLO SUPER HEALTHY MEAL</H1>
+            <a class="buttonlink" href="">I'm hungry</a>
+            <a class="buttonlink" href="">Show me more</a>
+        </div>
+    </header>
+    <div>
+        <h>Get food fast — not fast food.</h>
+        <p>Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!</p>
+    </div>
+    <div>
+        <span>
+            <h1>Up to 365 days/year</h1>
+            <p>Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!</p>
+        </span>
+        <span>
+            <h1>Ready in 20 minutes</h1>
+            <p></p>
+        </span>
+        <span>
+            <h1>100% organic</h1>
+            <p></p>
+        </span>
+        <span>
+            <h1>Order anything</h1>
+            <p></p>
+        </span>
+    </div>
+</body>
+</html>
