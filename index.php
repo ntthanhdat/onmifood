94989763b79ac1f31626e1274743992384acbd5f
= <=>
2020-11-26 01:39:12 +0700

    make website by guide from course's video

--- index.php
+++ index.php
@@ -5,48 +5,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Onmi Food</title>
     <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="Grid.css">
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     
 </head>
 <body>
-    <header>
+    <header> <!--
         <div class="topnav" >
             <a href="">FOOD DELIVERY</a>
-            <a href="">HOW IT WORKS</a>
+            <a href="sec2">HOW IT WORKS</a>
             <a href="">OUR CITIES</a>
             <a href="">SIGN UP</a>
         </div>
         <img src="image/logo-white.png" alt="logo" width="100vh" height="100vh" class="logo">
-            
+-->
+        <div>
+            <img src="image/logo-white.png" alt="logo" width="100px" height="auto" class="logo2">
+            <ul class="main-nav">
+                <li><a href="">FOOD DELIVER</a></li>
+                <li><a href="">HOW IT WORKS</a></li>
+                <li><a href="">OUR CITIES</a></li>
+                <li><a href="">SIGN UP</a></li>
+            </ul>
+        </div> 
         <div class="link">
             <H1 class="whiteh">GOODBYE JUNK FOOD. <br>HELLO SUPER HEALTHY MEAL</H1>
             <a class="buttonlink" href="">I'm hungry</a>
             <a class="buttonlink" href="">Show me more</a>
         </div>
     </header>
-    <div>
-        <h>Get food fast — not fast food.</h>
-        <p>Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!</p>
+    <div class="home">
+
+        <div>
+            <h1 class="blackh">Get food fast — not fast food.</h1>
+            <p class="underline"></p>
+            <p>Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. <br>No time for cooking. So let us take care of that, we’re really good at it, we promise!</p>
+        </div>
+        <div>
+            <span>
+                <h1>Up to 365 days/year</h1>
+                <p>Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!</p>
+            </span>
+            <span>
+                <h1>Ready in 20 minutes</h1>
+                <p></p>
+            </span>
+            <span>
+                <h1>100% organic</h1>
+                <p></p>
+            </span>
+            <span>
+                <h1>Order anything</h1>
+                <p></p>
+            </span>
+        </div>
     </div>
-    <div>
-        <span>
-            <h1>Up to 365 days/year</h1>
-            <p>Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!</p>
-        </span>
-        <span>
-            <h1>Ready in 20 minutes</h1>
-            <p></p>
-        </span>
-        <span>
-            <h1>100% organic</h1>
-            <p></p>
-        </span>
-        <span>
-            <h1>Order anything</h1>
-            <p></p>
-        </span>
+    <section id="sec2">
+        <h1>How it works - Simple as 1, 2, 3</h1>
+        <p>Choose the subscription plan that best fits your needs and sign up today.</p>
+        <p>Order your delicious meal using our mobile app or website. Or you can even call us!</p>
+        <p>Enjoy your meal after less than 20 minutes. See you the next time!</p>
+    </section>
+    <div class="row"> 
+        <h1>noidung</h1>
     </div>
 </body>
 </html>
